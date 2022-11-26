f7929c8d365b6ed7ff58c0fafdf093c29ffc71a3
MMuslimAbdulJ <mmuslimabdulj@gmail.com>
2022-11-26 14:12:01 +0700

    Add default value

--- src/Customer.php
+++ src/Customer.php
@@ -4,9 +4,8 @@ class Customer {
     public function __construct(private string $name) {
     }
     
-    public function sayHello(string $name) : string {
+    public function sayHello(string $name = "Guest") : string {
         return "Hello $name, my name is $this->name";
     }
-        
 }
 ?>
